230a456c71b67ca7a93fba833df8f6226c591d6e
sankar-talatam <119839082+sankar-talatam@users.noreply.github.com>
2023-08-18 12:02:03 +0530

    import java.util.Scanner; public class c{     public static void main(String[] argss){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=1;i<=n;i++)         {             System.out.print(i+" ");         }         } }

---
+++ N_natural_numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class c{
+    public static void main(String[] argss){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            System.out.print(i+" ");
+        }
+        }
+}
