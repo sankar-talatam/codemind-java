73b8119c144b750c1b41c39e719b05b972a1870f
sankar-talatam <119839082+sankar-talatam@users.noreply.github.com>
2023-08-25 11:36:11 +0530

    import java.util.*; public class c{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int sum=0;         for(int i=n;i>=1;i--)         {         System.out.printf("%d ",2*i);         }
        }
    }

---
+++ First_N_even_Natural_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class c{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=n;i>=1;i--)
+        {
+        System.out.printf("%d ",2*i);
+        }
+        
+    }
+}
