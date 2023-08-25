bf6d7a764d681abe494af0e993bca5a3f229442e
sankar-talatam <119839082+sankar-talatam@users.noreply.github.com>
2023-08-25 15:32:11 +0530

    import java.util.Scanner; public class c{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         float a=sc.nextFloat();         float b=sc.nextFloat();         System.out.printf("%.2f", a*b);
        }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class c{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextFloat();
+        float b=sc.nextFloat();
+        System.out.printf("%.2f", a*b);
+        
+    }
+}
