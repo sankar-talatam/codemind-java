4e484671398d0e25943e3611a9f1bd48fda6585f
sankar-talatam <119839082+sankar-talatam@users.noreply.github.com>
2023-08-26 10:36:47 +0530

    import java.util.Scanner; public class c{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         double a=3.14*n*n;         System.out.printf("%.2f",a);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class c{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        double a=3.14*n*n;
+        System.out.printf("%.2f",a);
+    }
+}
