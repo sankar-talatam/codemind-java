0ca8c30b60bb61ea087df6887b377afb7f8c12a8
sankar-talatam <119839082+sankar-talatam@users.noreply.github.com>
2023-09-01 09:46:53 +0530

    import java.util.*; public class c{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double c=sc.nextInt();         double f=32+(c*1.8);         System.out.printf("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class c{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double c=sc.nextInt();
+        double f=32+(c*1.8);
+        System.out.printf("%.2f",f);
+    }
+}
