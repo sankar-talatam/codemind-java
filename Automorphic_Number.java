01ac878b8de99555b4e90154197592725c26a6a3
sankar-talatam <119839082+sankar-talatam@users.noreply.github.com>
2023-09-07 11:02:35 +0530

    import java.util.*; public class c{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int s=n*n;         String s1=Integer.toString(n);         String s2=Integer.toString(s);         System.out.print(s2.endsWith(s1)?"Automorphic Number" : "Not an Automorphic Number");     } }

--- Automorphic_Number.java
+++ Automorphic_Number.java
@@ -3,28 +3,9 @@ public class c{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int t=n*n;
-        boolean flag = true;
-        while(n!=0)
-        {
-             if(n%10==t%10)
-             {
-                 n/=10;
-                 t/=10;
-             }
-             else
-             {
-                 flag=false;
-                 break;
-             }
-        }
-        if(flag)
-        {
-            System.out.println("Automorphic Number");
-        }
-        else
-        {
-            System.out.println("Not an Automorphic Number");
-        }
+        int s=n*n;
+        String s1=Integer.toString(n);
+        String s2=Integer.toString(s);
+        System.out.print(s2.endsWith(s1)?"Automorphic Number" : "Not an Automorphic Number");
     }
 }
